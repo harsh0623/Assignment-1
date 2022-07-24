2eed009b5bcf8a1b05488a279df55c972bc9eb3a
harsh0623 <hv3445393@gmail.com>
2022-07-24 16:11:15 +0530

    First commit-Assignments upload

---
+++ Assignment-3/Assignment3.js
@@ -0,0 +1,52 @@
+let students=[{
+    name:"Harsh",
+    id:1,
+    marks:{english:74,maths:95,science:67}
+},
+    {name:"Yash",
+    id:2,
+    marks:{english:64,maths:35,science:38}
+},
+   { name:"Hemant",
+    id:3,
+    marks:{english:98,maths:55,science:27}
+},
+    {name:"Saksham",
+    id:4,
+    marks:{english:68,maths:45,science:19}
+},
+    {name:"Saket",
+    id:5,
+    marks:{english:20,maths:69,science:55}
+},
+    {name:"Sumit",
+    id:6,
+    marks:{english:56,maths:37,science:67}
+},
+    {name:"Prashant",
+    id:7,
+    marks:{english:54,maths:67,science:78}
+},
+   { name:"Rohan",
+    id:8,
+    marks:{english:48,maths:75,science:87}
+},
+    {name:"Rohit",
+    id:9,
+    marks:{english:58,maths:77,science:32}
+},
+    {name:"Naveen",
+    id:10,
+    marks:{english:30,maths:25,science:31}
+}];
+for(let i=0;i<students.length;i++){
+students[i].averagemarks=(students[i].marks.english + students[i].marks.maths + students[i].marks.science)/3;
+students[i].result=students[i].averagemarks>=33
+ && students[i].marks.english>=33
+ && students[i].marks.math>=33 
+ && students[i].marks.science>=33 ? "Pass" :"Fail";
+console.log(students[i])};
+
+for(let student of students){
+    console.log(student);
+}
